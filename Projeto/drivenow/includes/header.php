4e34a1a620096ab338555f37a9c6adfe3cdb5971
EduardoBetiol <fakejapaniese@gmail.com>
2025-05-06 11:09:03 -0300

    Update header.php
    alteração no header

--- Projeto/drivenow/includes/header.php
+++ Projeto/drivenow/includes/header.php
@@ -8,7 +8,14 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>DriveNow</title>
+
+    <!-- Bootstrap -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Remix Icons -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/remixicon/4.5.0/remixicon.css">
+
+    <!-- Custom CSS -->
     <link href="assets/css/style.css" rel="stylesheet">
 </head>
 <body class="d-flex flex-column min-vh-100">
@@ -22,26 +29,51 @@
             <?php else: ?>
                 <a class="navbar-brand" href="./index.html">DriveNow</a>
             <?php endif; ?>
+
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav">
                 <span class="navbar-toggler-icon"></span>
             </button>
+
             <div class="collapse navbar-collapse" id="navbarNav">
-                <ul class="navbar-nav ms-auto">
+                <ul class="navbar-nav me-auto">
                     <?php if (estaLogado()): ?>
-                        <?php if ($paginaAtual != 'dashboard.php'): ?>
-                            <li class="nav-item">
-                                <a class="nav-link" href="../dashboard.php">Dashboard</a>
-                            </li>
-                        <?php endif; ?>
+                        <!-- Links à esquerda para usuários logados -->
                         <li class="nav-item">
-                            <?php if (
-                                str_contains($caminhoAtual, 'veiculo/') ||
-                                str_contains($caminhoAtual, 'perfil/')
-                            ): ?>
-                                <a class="nav-link" href="../logout.php">Sair</a>
-                            <?php else: ?>
-                                <a class="nav-link" href="./logout.php">Sair</a>
-                            <?php endif; ?>
+                            <a class="nav-link" href="#">Home</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">Sobre</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">Carros</a>
+                        </li>
+                    <?php endif; ?>
+                </ul>
+
+                <ul class="navbar-nav ms-auto align-items-center">
+                    <?php if (estaLogado()): ?>
+                        <!-- Dropdown de perfil -->
+                        <li class="nav-item dropdown">
+                            <a class="nav-link dropdown-toggle d-flex align-items-center" href="#" id="perfilDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                                <?= htmlspecialchars($usuario['primeiro_nome']) ?>
+                            </a>
+                            <ul class="dropdown-menu dropdown-menu-end" aria-labelledby="perfilDropdown">
+                                <li><a class="dropdown-item" href="#"><i class="ri-user-line me-2"></i>Perfil</a></li>
+                                <li><a class="dropdown-item" href="#"><i class="ri-time-line me-2"></i>Atividades</a></li>
+                                <li><a class="dropdown-item" href="#"><i class="ri-bookmark-line me-2"></i>Salvos</a></li>
+                                <li><a class="dropdown-item" href="#"><i class="ri-settings-3-line me-2"></i>Configurações</a></li>
+                                <li><hr class="dropdown-divider"></li>
+                                <li>
+                                    <?php if (
+                                        str_contains($caminhoAtual, 'veiculo/') ||
+                                        str_contains($caminhoAtual, 'perfil/')
+                                    ): ?>
+                                        <a class="dropdown-item" href="../logout.php"><i class="ri-logout-box-r-line me-2"></i>Sair</a>
+                                    <?php else: ?>
+                                        <a class="dropdown-item" href="./logout.php"><i class="ri-logout-box-r-line me-2"></i>Sair</a>
+                                    <?php endif; ?>
+                                </li>
+                            </ul>
                         </li>
                     <?php else: ?>
                         <li class="nav-item">
